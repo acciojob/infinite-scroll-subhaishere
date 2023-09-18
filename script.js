77e25c1a2fe1cf783c41ac414f534b5510bba3e7
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-09-18 21:30:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const con = document.getElementById("infi-list");
 
+let itemNo = 1;
+const addList = (num)=>{
+	for(let i=1; i<= num; i++){
+		const ele = document.createElement("li");
+		ele.innerHTML = `Item ${itemNo}`;
+		con.append(ele);
+		itemNo++;
+	}
+}
+con.onscroll = ()=>{
+	if(Math.ceil(con.clientHeight + con.scrollTop) >= con.scrollHeight){
+		addList(2);
+	}
+}
+
+
+
+addList(10);
